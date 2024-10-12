4fbc2919e007c7475ea20ade6b098c94ec99cba1
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2024-10-11 21:18:03 -0300

    styling itens

--- index.php
+++ index.php
@@ -41,7 +41,26 @@ if (isset($_POST['search'])) {
     $result = searchArray($data, $input);
 }
 ?>
-
+<style>
+  .external-content{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  .external-content .content{
+    border: 1px solid black;
+    margin: 10px;
+    width: 500px;
+    padding: 8px;
+  }
+  .external-content .social_media{
+    display: flex;
+    justify-content: space-between;
+  }
+  .img_avatar{
+    border-radius: 50%;
+  }
+</style>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -62,19 +81,51 @@ if (isset($_POST['search'])) {
         <input type="text" name="search" placeholder="Type a name..." required>
         <button type="submit">Search</button>
     </form>
-
-    <?php if ($result !== null): ?>
-        <h2>Results:</h2>
-        <?php if (count($result) > 0): ?>
-            <ul>
-                <?php foreach ($result as $item): ?>
-                    <li><?php echo htmlspecialchars($item['title']); ?></li>
-                <?php endforeach; ?>
-            </ul>
-        <?php else: ?>
-            <p>No matches found.</p>
-        <?php endif; ?>
-    <?php endif; ?>
+    <!-- begin - search results -->
+    <h2>Results:</h2>
+        <div class="external-content">
+            <?php if ($result !== null): ?>
+                <?php if (count($result) > 0): ?>
+                    <?php foreach ($result as $item): ?>
+                        <div class="content">
+                            <p><?php echo htmlspecialchars($item['title']); ?></p>
+                            <!--  -->
+                            <a href="<?php echo $item['url'] ;?>">
+                                <p>Titulo: <?php echo $item['title'] ;?></p>
+                            </a>
+                            <p class="">Id: <?php echo $item['id'] ;?></p>
+                            <?php           
+                                $new_image = $item['images']['480w_still']['url'];            
+                                $image_new = substr($new_image, 0, -5);
+                            ;?>
+                            
+                            <img src="<?php echo $image_new ;?>" width="270px" height="270px">
+                            <br><br>
+                            <hr>
+                            <?php $user_data = $item['user']['username'];?>
+                            
+                                <?php
+                                if($user_data){?>
+                                    <p>User: <?php echo $user_data;?></p>
+                                    <?php 
+                                }          
+                                ;?>
+                            <p>Avatar: <img class="img_avatar" src="<?php echo $item['user']['banner_image'] ;?>" width="50px" height="50px"></p>
+                            <p>Description: <?php echo $item['user']['description'] ;?></p>
+                            <p>Social media: </p>
+                            <div class="social_media">
+                                <a href="<?php echo $item['user']['profile_url'] ;?>">Portfolio </a> 
+                                <a href="<?php echo $item['user']['instagram_url'] ;?>">Instagram </a> 
+                                <a href="<?php echo $item['user']['website_url'] ;?>">Website </a> 
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <p>No matches found.</p>
+                <?php endif; ?>
+            <?php endif; ?>
+        </div>
+    <!-- end - search results -->
 
 </body>
 </html>
